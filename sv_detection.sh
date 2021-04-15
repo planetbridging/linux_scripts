cd1cb2daa189c8d4bcddf77a1530daad5c5ba32a
planetbridging <47130739+planetbridging@users.noreply.github.com>
2021-04-15 11:25:01 +1000

    Change folder saves

--- sv_detection.sh
+++ sv_detection.sh
@@ -15,9 +15,9 @@
 
 open_ports=()
 
-ports_ss=$(eval nmap -sV -vv -p $2 -sS -oG - $1 | awk '/open/{ s = ""; for (i = 5; i <= NF-4; i++) s = s substr($i,1,length($i)-4) "\n"; print $2 " " $3 "\n" s}')
-ports_st=$(eval nmap -sV -vv -p $2 -sT -oG - $1 | awk '/open/{ s = ""; for (i = 5; i <= NF-4; i++) s = s substr($i,1,length($i)-4) "\n"; print $2 " " $3 "\n" s}')
-ports_sa=$(eval nmap -sV -vv -p $2 -sA -oG - $1 | awk '/open/{ s = ""; for (i = 5; i <= NF-4; i++) s = s substr($i,1,length($i)-4) "\n"; print $2 " " $3 "\n" s}')
+ports_ss=$(eval nmap -sV -vv -p $2 -sS -oX sv_detection/$1_ss.txt $1 | awk '/open/{ s = ""; for (i = 5; i <= NF-4; i++) s = s substr($i,1,length($i)-4) "\n"; print $2 " " $3 "\n" s}')
+ports_st=$(eval nmap -sV -vv -p $2 -sT -oX sv_detection/$1_st.txt $1 | awk '/open/{ s = ""; for (i = 5; i <= NF-4; i++) s = s substr($i,1,length($i)-4) "\n"; print $2 " " $3 "\n" s}')
+ports_sa=$(eval nmap -sV -vv -p $2 -sA -oX sv_detection/$1_sa.txt $1 | awk '/open/{ s = ""; for (i = 5; i <= NF-4; i++) s = s substr($i,1,length($i)-4) "\n"; print $2 " " $3 "\n" s}')
 
 echo $ports_ss
 echo $ports_st
